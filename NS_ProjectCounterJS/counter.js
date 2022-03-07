a17650ebce5a4461108c58c32694fc1c536e5952
Tripti Sen <97985299+trips13@users.noreply.github.com>
2022-03-07 19:33:05 +0530

    Add files via upload

---
+++ NS_ProjectCounterJS/counter.js
@@ -0,0 +1,18 @@
+let increment = document.querySelector("#increment");
+let decrement = document.querySelector("#decrement");
+let counterValue = document.querySelector("#counterValue");
+
+let counter = 0;
+
+increment.addEventListener("click" , ()=>{
+    counter++;
+    counterValue.innerText = counter;
+});
+decrement.addEventListener("click" , ()=>{
+    counter--;
+    counterValue.innerText = counter;
+});
+reset.addEventListener("click" , ()=>{
+    counter = 0;
+    counterValue.innerText = counter;
+});
